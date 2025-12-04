952cac780796efa80b4d18a682882d2c0383699a
joshilashok <103981048+joshilashok@users.noreply.github.com>
2025-12-04 14:10:25 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,15 +1,8 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const port = 3000;
 
-// Serve static files from the "public" folder
-app.use(express.static('public'));
-
-// Main route
-app.get('/', (req, res) => {
-    res.sendFile(__dirname + '/public/index.html');
+app.get("/", (req, res) => {
+  res.send("Hello from Node App on ECS!");
 });
 
-app.listen(3000, '0.0.0.0', () => {
-    console.log(`Server is running at http://0.0.0.0:${port}`);
-});
+app.listen(3000, () => console.log("Server running on port 3000"));
